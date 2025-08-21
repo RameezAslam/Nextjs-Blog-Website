8ba289b388e58a86dc8ba78dfe30258e1a0dff39
Rameez Aslam <rameezaslam387@gmail.com>
2025-08-21 15:55:39 +0500

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+      remotePatterns: [
+        {
+           hostname: "encrypted-tbn0.gstatic.com",
+           protocol: "https",
+           pathname: "/**",
+           port: "", 
+        },
+         {
+           hostname: "images.pexels.com",
+           protocol: "https",
+           pathname: "/**",
+           port: "", 
+        },
+        {
+          hostname: "lh3.googleusercontent.com",
+          protocol: "https",
+          pathname: "/**",
+          port: "",
+        }
+      ]
+   }
 };
 
 export default nextConfig;
